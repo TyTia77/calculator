4c6d421bd1ad325947b8c9606fd908d5ea6be503
Ty Tia <tytia77@gmail.com>
2017-05-12 17:54:23 +1000

    added functionality and some ui changes

--- script.js
+++ script.js
@@ -1,22 +1,126 @@
-var app = angular.module('calculator', []);
+// ngSanitize allows html entities in angular string
+var app = angular.module('calculator', ['ngSanitize']);
 
 app.controller('mainCtrl', ['$scope', function($scope){
+
+initButton();
+
+$scope.screen = '';
+var entry = '';
+$scope.keyPress = function(ev){
+    var regexp = /\d/;
+    var regendnum = /\d$/;
+    // $scope.screen += ev.target.dataset.value;
+
+    var keyPressed = ev.target.dataset.value;
+
+    // console.log(regexp.test(keyPressed));
+
+    if (regexp.test(keyPressed)){
+        $scope.screen += keyPressed;
+        entry += keyPressed;
+    }
+    else {
+        switch (keyPressed){
+            case 'ac':
+            case 'ce':
+                buttonFunctions.clearAll();
+                break;
+
+            case 'rt':
+                entry = Math.sqrt(parseFloat(entry));
+                $scope.screen = entry;
+                break;
+
+            case '%':
+                entry /= 100;
+                $scope.screen /= 100;
+                break;
+
+            case '+':
+                entry += '+';
+                $scope.screen += '+';
+                break;
+
+            case '/':
+                entry += '/';
+                $scope.screen += '&#247;';
+                console.log($scope.screen);
+                break;
+
+            case '-':
+                entry += '-';
+                $scope.screen += '-';
+                break;
+
+            case '*':
+                entry += '*';
+                $scope.screen += 'x';
+                break;
+
+            case '=':
+                if (regendnum.test(entry)){
+                    buttonFunctions.calculate(entry);
+                }
+                break;
+
+            default:
+                console.log('not valid');
+                break;
+        }
+    }
+
+    ev.target.classList.add('press');
+    setTimeout(function(){
+        ev.target.classList.remove('press');
+    }, 100);
+}
+
+
+var buttonFunctions = {
+     clearAll: function(){
+        entry = '';
+        clearScreen();
+    },
+
+    //TODO
+    clearEntry: function(){
+
+    },
+
+    calculate: function(entry){
+        entry = eval(entry);
+        $scope.screen = entry;
+        console.log('screen', $scope.screen);
+    }
+};
+
+//TODO
+function displayScreen(){
+
+}
+
+function clearScreen(){
+    $scope.screen = '';
+}
+
+function initButton(){
     $scope.btn = [
         [
-            { label: 'ac', value: 'ac' },
-            { label: 'ce',value: 'ce' },
-            { label:'rt', value: 'rt' },
+            { label: 'AC', value: 'ac' },
+            { label: 'CE',value: 'ce' },
+            { label:'&#8730;', value: 'rt' },
             { label:'%', value: '%'}
         ],[
             { label:'mrc', value: 'mrc' },
             { label:'m-', value: 'm-' },
             { label:'m+', value: 'm+' },
-            { label:'x', value: 'x' }
+            { label:'x', value: '*' }
         ], [
             { label:'7', value: 7 },
             { label:'8', value: 8 },
             { label:'9', value: 9 },
-            { label:'/', value: '/' }
+            { label:'&#247;', value: '/' }
         ], [
             { label:'4', value: 4 },
             { label:'5', value: 5 },
@@ -34,11 +138,7 @@ app.controller('mainCtrl', ['$scope', function($scope){
             { label:'=', value: '=' }
         ]
     ];
-
-$scope.keyPress = function(ev){
-    ev.target.classList.add('press');
-    setTimeout(function(){
-        ev.target.classList.remove('press');
-    }, 100);
 }
+
+//TODO end controller
 }]);
